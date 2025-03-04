cbd9e759374ef0bdb9b649e1a6199b3acfe86506
Def-228 <n2520687@gmail.com>
2025-03-04 19:42:31 +0600

    sosali?

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router";
-import { HomePage } from "../pages/HomePage";
-import { SignIn } from "../pages/SignIn";
-import { SignUp } from "../pages/SignUp";
+import { HomePage } from "./pages/HomePage";
+import { SignIn } from "pages/SignIn";
+import { SignUp } from "/pages/SignUp";
 import { ProductsPage } from "../pages/ProductsPage";
 import { TopHeader } from "../components/TopHeader";
 import { ChangeLang } from "../components/ChangeLang";
